cc3d00779813f2f97680b1b9c01b4d9ebea8f80a
Tarun Dutt <tarun.dutta499@gmail.com>
2026-07-10 16:15:22 +0530

    feat: Scaffold Chrome Extension directory chrome-extension/ for developer review

---
+++ chrome-extension/background.js
@@ -0,0 +1,4 @@
+// Background service worker for Nexaviq Chrome Extension
+chrome.runtime.onInstalled.addListener(() => {
+  console.log("Nexaviq Agent Co-Pilot Extension successfully installed.");
+});
---
+++ chrome-extension/content.js
@@ -0,0 +1,2 @@
+// Content script to stream or inject widgets into dialer pages (Genesys, Twilio, etc.)
+console.log("[Nexaviq] Dialer content page connector loaded.");
